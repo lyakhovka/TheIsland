f32f4e5a47e381d7e3f786ab4d61851d50e52d1b
Olena Borodych <olenaborodych@gmail.com>
2024-03-28 23:37:22 +0200

    add some properties and methods to Plant

--- Plant.java
+++ Plant.java
@@ -1,10 +1,15 @@
 public class Plant extends Alive{
+    public static final String ICON = "\uD83C\uDF31";
     public static final double MIN_PLANT_WEIGHT = 0.1; //amount of food for the smallest herbivore
     public static final double MAX_PLANT_WEIGHT = 60; //amount of food for the biggest herbivore
     public double weight;
 
     public Plant(){
         this.weight = Math.random() * (MAX_PLANT_WEIGHT - MIN_PLANT_WEIGHT) + MIN_PLANT_WEIGHT;
+    }
+
+    public void paint(){
+        System.out.print(ICON);
     }
 
 }
