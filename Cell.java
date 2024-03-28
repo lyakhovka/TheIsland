a4bbb44febbe6916df1a57b1bb28fe34ed0282c1
Olena Borodych <olenaborodych@gmail.com>
2024-03-28 23:38:03 +0200

    implement cell and island initialization

--- Cell.java
+++ Cell.java
@@ -1,2 +1,78 @@
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
+
 public class Cell {
+
+    public List<Animal> animalsInCell = new ArrayList<>();
+    public List<Plant> plantsInCell = new ArrayList<>();
+
+ //   private final int SPECIES_TOTAL_AMOUNT = 16;
+
+    private Map<String, Integer> maxAliveAmountInCell = getMaxAliveAmountInCell();
+
+    private Map<String, Integer> getMaxAliveAmountInCell() {
+        Map<String, Integer> maxAmount = new HashMap<>();
+        maxAmount.put("Wolf", 30);
+//        maxAmount.put("Boa", 30);
+//        maxAmount.put("Fox", 30);
+        maxAmount.put("Bear", 5);
+//        maxAmount.put("Eagle", 20);
+//        maxAmount.put("Horse", 20);
+//        maxAmount.put("Deer", 20);
+        maxAmount.put("Rabbit", 150);
+//        maxAmount.put("Mouse", 500);
+//        maxAmount.put("Goat", 140);
+//        maxAmount.put("Sheep", 140);
+//        maxAmount.put("Hog", 50);
+//        maxAmount.put("Buffalo", 10);
+//        maxAmount.put("Duck", 200);
+//        maxAmount.put("Caterpillar", 1000);
+        maxAmount.put("Plant", 200);
+
+        return maxAmount;
+    }
+
+    public Cell() {
+        for (String aliveName : maxAliveAmountInCell.keySet()) {
+            Random random = new Random();
+            int aliveAmount = random.nextInt(maxAliveAmountInCell.get(aliveName));
+            for (int i = 0; i < aliveAmount; i++) {
+                try {
+                    Object newAlive = Class.forName(aliveName).getDeclaredConstructor().newInstance();
+                    if (newAlive instanceof Animal) {
+                        animalsInCell.add((Animal) newAlive);
+                    } else if (newAlive instanceof Plant) {
+                        plantsInCell.add((Plant) newAlive);
+                    }
+
+                } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                         NoSuchMethodException | ClassNotFoundException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+
+        }
+    }
+
+    public void reportStatus() {
+
+        System.out.println("ANIMALS_TOTAL_AMOUNT: " + animalsInCell.size());
+
+        for (int i = 0; i < animalsInCell.size(); i++) {
+            animalsInCell.get(i).paint();
+           // System.out.print("hp: " + animalsInCell.get(i).hp + " weight: " + animalsInCell.get(i).weight + " speed: " + animalsInCell.get(i).maxSpeed + " foodAmount: " + animalsInCell.get(i).maxFoodAmount);
+//            System.out.println("Possible preys: ");
+//            for (String prey : animalsInCell.get(i).possiblePreys.keySet()) {
+//                System.out.println("prey: " + prey + " probability: " + animalsInCell.get(i).possiblePreys.get(prey));
+//            }
+        }
+        System.out.println("");
+        System.out.println("PLANTS_TOTAL_AMOUNT: " + plantsInCell.size());
+        for (int i = 0; i < plantsInCell.size(); i++) {
+            plantsInCell.get(i).paint();
+//            System.out.println();
+//            System.out.println("weight: " + plantsInCell.get(i).weight);
+        }
+    }
+
 }
--- Island.java
+++ Island.java
@@ -1,3 +1,29 @@
 public class Island {
     public Cell[][] cells;
+    private int islandWidth;
+    private int islandHeight;
+
+    public Island (int width, int height) {
+        islandWidth = width;
+        islandHeight = height;
+
+        cells = new Cell[islandWidth][islandHeight];
+        for (int i = 0; i < islandWidth; i++) {
+            for (int j = 0; j < islandHeight; j++) {
+                cells[i][j] = new Cell();
+            }
+        }
+        for (int i = 0; i < islandWidth; i++) {
+            for (int j = 0; j < islandHeight; j++) {
+                System.out.println("");
+                System.out.println("");
+                System.out.println("CELL["+i+"]["+j+"]");
+                cells[i][j].reportStatus();
+            }
+
+        }
+    }
+    public static void main(String[] args){
+        new Island(2, 2);
+    }
 }
