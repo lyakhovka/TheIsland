af135e8dfc553ef5a6016542a8d49862181b2c56
Olena Borodych <olenaborodych@gmail.com>
2024-04-03 23:21:46 +0300

    tried to add synchronization to 'run'

--- Carnivore.java
+++ Carnivore.java
@@ -1,21 +1,20 @@
 public abstract class Carnivore extends Animal {
-
+    private static final Object lock = new Object();
     @Override
     public void run() {
-        while(isAlive){
+        while(isCreatureAlive){
             int x = currentPosition.get("X");
             int y = currentPosition.get("Y");
-            for (Animal animal : Island.cells[x][y].animalsInCell) {
-                eat(animal);
-            }
-            for (Animal animal : Island.cells[x][y].animalsInCell) {
-                reproduce(animal);
-            }
-            move(setDestination());
+                synchronized (lock) {
+                    for (Animal animal : Island.cells[x][y].animalsInCell) {
+                        eat(animal);
+                    }
+                    for (Animal animal : Island.cells[x][y].animalsInCell) {
+                        reproduce(animal);
+                    }
+                    move(setDestination());
 
-
+                }
         }
     }
-
-
 }
--- Herbivore.java
+++ Herbivore.java
@@ -1,11 +1,14 @@
 public abstract class Herbivore extends Animal{
+    private static final Object lock = new Object();
     @Override
     public void run() {
-        while(isAlive){
+        while(isCreatureAlive){
             int x = currentPosition.get("X");
             int y = currentPosition.get("Y");
-            for (Plant plant : Island.cells[x][y].plantsInCell) {
-                eat(plant);
+            synchronized (lock) {
+                for (Plant plant : Island.cells[x][y].plantsInCell) {
+                    eat(plant);
+                }
             }
             for (Animal animal : Island.cells[x][y].animalsInCell) {
                 reproduce(animal);
