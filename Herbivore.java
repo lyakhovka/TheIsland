01d08d9ad313e93bb78f99100fd87d77a01e847b
Olena Borodych <olenaborodych@gmail.com>
2024-04-03 21:30:28 +0300

    add designated 'run' methods for different types of animals

--- Herbivore.java
+++ Herbivore.java
@@ -1,3 +1,19 @@
 public abstract class Herbivore extends Animal{
+    @Override
+    public void run() {
+        while(isAlive){
+            int x = currentPosition.get("X");
+            int y = currentPosition.get("Y");
+            for (Plant plant : Island.cells[x][y].plantsInCell) {
+                eat(plant);
+            }
+            for (Animal animal : Island.cells[x][y].animalsInCell) {
+                reproduce(animal);
+            }
+            move(setDestination());
+
+
+        }
+    }
 
 }
--- Omnivore.java
+++ Omnivore.java
@@ -1,4 +1,22 @@
 public abstract class Omnivore extends Animal {
+    @Override
+    public void run() {
+        while(isAlive){
+            int x = currentPosition.get("X");
+            int y = currentPosition.get("Y");
+            for (Plant plant : Island.cells[x][y].plantsInCell) {
+                eat(plant);
+            }
+            for (Animal animal : Island.cells[x][y].animalsInCell) {
+                eat(animal);
+            }
+            for (Animal animal : Island.cells[x][y].animalsInCell) {
+                reproduce(animal);
+            }
+            move(setDestination());
 
+
+        }
+    }
 }
 
