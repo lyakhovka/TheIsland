17631763b1d5f3a0342a3e1c35b6bd3c377d46e0
Olena Borodych <olenaborodych@gmail.com>
2024-04-22 10:56:05 +0300

    add input parameters for island width and height; implement validation for them

--- src/main/java/runner/Main.java
+++ src/main/java/runner/Main.java
@@ -6,11 +6,24 @@ import static island.Island.cells;
 
 public class Main {
     public static void main(String[] args) {
-//        int islandWidth = Integer.parseInt(args[0]);
-//        int islandHeight = Integer.parseInt(args[1]);
+        int islandWidth;
+        int islandHeight;
+        try {
+           islandWidth = Integer.parseInt(args[0]);
+           islandHeight = Integer.parseInt(args[1]);
+           if (islandWidth <= 0 || islandHeight <= 0){
+               System.out.println("Please, specify correct width and height of the Island. Expected values are integers above zero.");
+               return;
+           }
+       } catch (NumberFormatException e){
+           System.out.println("Please, specify correct width and height of the Island. Expected values are integers above zero.");
+           return;
+       }
 
-        int islandWidth = 3;
-        int islandHeight = 3;
+
+
+//        int islandWidth = 3;
+//        int islandHeight = 3;
 
         Island theIsland = new Island(islandWidth, islandHeight);
         int roundNumber = 1;
