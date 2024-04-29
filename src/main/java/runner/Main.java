ee306fa7548e3785d1d10d006623969bd7e7f75e
Olena Borodych <olenaborodych@gmail.com>
2024-04-29 22:38:46 +0300

    implement input params validation instead of catching exception

--- src/main/java/runner/Main.java
+++ src/main/java/runner/Main.java
@@ -3,27 +3,37 @@ package runner;
 import island.Island;
 
 import static island.Island.cells;
+import static org.apache.commons.lang3.math.NumberUtils.isDigits;
 
 public class Main {
+
+    private static final String ERROR_LOG = """
+            Please, specify correct width and height of the Island.
+            Expected values are integers above zero.""";
+
+    public static void validateArgs(String[] args) {
+        int argNumber = args.length;
+        int heightParamIndex = 0;
+        int widthParamIndex = 1;
+
+        if ((argNumber < 2 || !isDigits(args[heightParamIndex]) || !isDigits(args[widthParamIndex]))) {
+
+            showInfoAndQuit();
+        }
+    }
+
+    private static void showInfoAndQuit() {
+        System.out.println(ERROR_LOG);
+        System.exit(1);
+    }
+
     public static void main(String[] args) {
         int islandWidth;
         int islandHeight;
-        try {
-           islandWidth = Integer.parseInt(args[0]);
-           islandHeight = Integer.parseInt(args[1]);
-           if (islandWidth <= 0 || islandHeight <= 0){
-               System.out.println("Please, specify correct width and height of the Island. Expected values are integers above zero.");
-               return;
-           }
-       } catch (NumberFormatException e){
-           System.out.println("Please, specify correct width and height of the Island. Expected values are integers above zero.");
-           return;
-       }
 
-
-
-//        int islandWidth = 3;
-//        int islandHeight = 3;
+        validateArgs(args);
+        islandWidth = Integer.parseInt(args[0]);
+        islandHeight = Integer.parseInt(args[1]);
 
         Island theIsland = new Island(islandWidth, islandHeight);
         int roundNumber = 1;
